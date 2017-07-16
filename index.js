0f3e48098848bcbc05e5aa6e289a8542f8dd256a
ralam87 <Ruhull.alam@outlook.com>
2017-07-16 18:40:43 +0100

    made data object into JSON formatted

--- index.js
+++ index.js
@@ -1,50 +1,3 @@
-const data = [
-  {name: 'Global',
-  tags: ['global', 'window'],
-  description: `Global is one of the main objects of NodeJS,
-  similar to what the window object is in the browser`,
-  alsoSee : [' process']},
-
-  {name: 'Process',
-  tags: ['process', 'document'],
-  description: `Process is one of the main objects of NodeJS,
-  similar to what the document object is in the browser`,
-  alsoSee : [' global']},
-
-  {name: 'Require',
-  tags: ['require', 'module'],
-  description: `Require is a function that allows us to split our
-  javascript into modules `,
-  alsoSee : [' module']},
-
-  {name: 'FS',
-  tags: ['fs', 'module', 'file', 'system'],
-  description: `fs is one of inbuilt modules in NodeJS, it allows us to
-  access the file system on the local computer`,
-  alsoSee : [' fs.appendFile',' module']},
-
-  {name: 'fs.appendFile',
-  tags: ['append', 'module', 'file', 'system'],
-  description: `Using this function from the fs module, you can create simple
-  files, or amend existing ones. The first argument (string) is the name of
-  the file you want to append, the second (string) is the data you want to
-  add to that. the third is a callback function you can add  `,
-  alsoSee : [' fs',' module']},
-
-  {name: 'OS',
-  tags: ['os', 'module', 'operating', 'system'],
-  description: `os is is one of inbuilt modules in NodeJS, it allows us to
-  access the operating system information on the local computer`,
-  alsoSee : [' os.userInfo',' module']},
-
-  {name: 'os.userInfo',
-  tags: ['user', 'module', 'operating', 'system', 'info'],
-  description: `By using this function from the os module you can access
-  certain properties about the user of the local system, such as their username
-  It doesn't require any arguments and will return an object with properties`,
-  alsoSee : [' os',' module']}
-]
-
 let searchButton = document.querySelector('.searchButton')
 let searchValue = document.querySelector('.main-search__text')
 let displayInfoDiv = document.querySelector('#infoDisplay')
@@ -96,3 +49,40 @@ function search() {
     })
   })
 }
+
+const data = [
+  {"name": "Global",
+  "tags": ["global", "window"],
+  "description" : "Global is one of the main objects of NodeJS similar to what the window object is in the browser",
+  "alsoSee" : [" process"]},
+
+  {"name": "Process",
+  "tags": ["process", "document"],
+  "description": "Process is one of the main objects of NodeJS similar to what the document object is in the browser",
+  "alsoSee" : [" global"]},
+
+  {"name": "Require",
+  "tags": ["require", "module"],
+  "description": "Require is a function that allows us to split our javascript into modules ",
+  "alsoSee" : [" module"]},
+
+  {"name": "FS",
+  "tags": ["fs", "module", "file", "system"],
+  "description": "fs is one of inbuilt modules in NodeJS, it allows us to access the file system on the local computer",
+  "alsoSee" : [" fs.appendFile"," module"]},
+
+  {"name": "fs.appendFile",
+  "tags": ["append", "module", "file", "system"],
+  "description": "Using this function from the fs module, you can create simple files, or amend existing ones. The first argument (string) is the name of the file you want to append, the second (string) is the data you want to add to that. the third is a callback function you can add",
+  "alsoSee" : [" fs"," module"]},
+
+  {"name": "OS",
+  "tags": ["os", "module", "operating", "system"],
+  "description": "os is is one of inbuilt modules in NodeJS, it allows us to access the operating system information on the local computer",
+  "alsoSee" : [" os.userInfo"," module"]},
+
+  {"name": "os.userInfo",
+  "tags": ["user", "module", "operating", "system", "info"],
+  "description": "By using this function from the os module you can access certain properties about the user of the local system, such as their username It doesn't require any arguments and will return an object with properties",
+  "alsoSee" : [" os"," module"]}
+]
